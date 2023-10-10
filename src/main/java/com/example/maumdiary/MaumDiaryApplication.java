bed35a18f394c67c3dcdf3d02006e4039ad724d7
HyeonJu LEE <140503027+Leehyeonju0219@users.noreply.github.com>
2023-10-10 15:30:32 +0900

    [INIT]

---
+++ src/main/java/com/example/maumdiary/MaumDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.maumdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaumDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaumDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/maumdiary/MaumDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maumdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaumDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
