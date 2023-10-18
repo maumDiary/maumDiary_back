e0c2ff259190f5d5c1c1c36d33760e805c482431
LHeon0310 <117696110+LHeon0310@users.noreply.github.com>
2023-10-18 22:28:39 +0900

    Destination Update
    Change maumDiary_back -> deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,18 +2,18 @@
 
 BUILD_JAR=$(ls /home/ubuntu/maumDiary_back/build/libs*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
-echo ">>> build 파일명: $JAR_NAME" >> /home/ubuntu/maumDiary_back/deploy.log
+echo ">>> build 파일명: $JAR_NAME" >> /home/ubuntu/deploy/deploy.log
 
-echo ">>> build 파일 복사" >> /home/ubuntu/maumDiary_back/deploy.log
-DEPLOY_PATH=/home/ubuntu/maumDiary_back/
+echo ">>> build 파일 복사" >> /home/ubuntu/deploy/deploy.log
+DEPLOY_PATH=/home/ubuntu/deploy/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo ">>> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/maumDiary_back/deploy.log
+echo ">>> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/deploy/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/maumDiary_back/deploy.log
+  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/deploy/deploy.log
 else
   echo ">>> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
@@ -21,5 +21,5 @@ else
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo ">>> DEPLOY_JAR 배포"    >> /home/ubuntu/maumDiary_back/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/maumDiary_back/deploy_err.log &
+echo ">>> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy/deploy.log
+nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/deploy/deploy_err.log &
