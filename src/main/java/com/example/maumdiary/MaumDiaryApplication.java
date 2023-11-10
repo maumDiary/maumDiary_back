b01312e9a2365e0e15c426d2afe339efe69e2039
HyeonJu LEE <140503027+Leehyeonju0219@users.noreply.github.com>
2023-11-10 14:48:16 +0900

    [UPDATE] TimeZone을 서울로 지정

--- src/main/java/com/example/maumdiary/MaumDiaryApplication.java
+++ src/main/java/com/example/maumdiary/MaumDiaryApplication.java
@@ -3,8 +3,16 @@ package com.example.maumdiary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class MaumDiaryApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MaumDiaryApplication.class, args);
